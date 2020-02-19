301b23f50098ad2cb50f507e0889e0f841d58f88
plsmaop <allenivan@gmail.com>
2020-02-19 21:28:42 +0800

    finish 10085

--- 10085/job.c
+++ 10085/job.c
@@ -1,10 +1,12 @@
 #include "utils.h"
- 
+
+#define ASSUMED_CACHE_LINE_SIZE 16
+
 int ret[128];
 int run(int n, int key) {
+    f(n, key, ret, ret+ASSUMED_CACHE_LINE_SIZE, ret+2*ASSUMED_CACHE_LINE_SIZE, ret+3*ASSUMED_CACHE_LINE_SIZE);
     int sum = 0;
-    f(n, key, ret, ret+1, ret+2, ret+3);
-    for (int i = 0; i < 4; i++)
-        sum += ret[i];
+    for (int i = 0; i < 4; i++) sum += ret[i*ASSUMED_CACHE_LINE_SIZE];
+       
     return sum;
 }
